2bb648eed294cf228c397e145bcec7cb16051936
Alexander Nikolas Breuer <anbreuer@ucsd.edu>
2017-10-20 19:30:36 -0700

    Added support for samples and multiple components in TF_MISFITS_GOF wrapper.

--- tools/processing/tf_misfit_gof.sh
+++ tools/processing/tf_misfit_gof.sh
@@ -24,21 +24,23 @@
 # Usage info
 show_help() {
 cat << EOF
-Usage: ${0##*/} [-h] [-r RECEIVER1 -s RECEIVER2 -t NTS -u DT -e DROP1 -f DROP2 -x EXTQ1 -y EXTQ2 -c EXTC1 -d EXTC2 -n EXTS1 -m EXTS2 -o OUTDIR]
+Usage: ${0##*/} [-h] [-r RECEIVER1 -s RECEIVER2 -t NTS -u DT -e DROP1 -f DROP2 -x EXTQ1 -y EXTQ2 -m EXTS1 -n EXTS2 -o OUTDIR]
 Calls TF-MISFIT_GOF_CRITERIA to produce goodness-of-fit criteria.
      -h This help message.
-     -r RECEIVER1 first seismogram.
-     -s RECEIVER2 second seismogram, used as reference solution.
+     -r RECEIVER1 first seismogram, used as reference solution.
+     -s RECEIVER2 second seismogram.
      -t NTS number of time steps.
      -u DT time step.
      -e DROP1 drops the first DROP1-1 lines for the first receiver (optional).
      -f DROP2 drops the first DROP2-1 lines for the second receiver (optional).
-     -x EXTQ1 component to extract from first receiver (optional).
-     -y EXTQ2 component to extract from second receiver (optional).
-     -c EXTC1 fused simulation to extract from first receiver (optional).
-     -d EXTC2 fused simulation to extract from second receiver (optional).
-     -n EXTS1 sampling of first receiver (optional).
-     -m EXTS2 sampling of second receiver (optional).
+     -x EXTQ1 components to extract from first receiver (optional, time in column 0 is always parsed,
+        default component is 1); use multiple times, to extract multiple components,
+        e.g., -x 1 -x 3 to extract 1 and 3.
+     -y EXTQ2 components to extract from second receiver (optional, time in column 0 is always parsed,
+        default component is 1); use multiple times, to extract multiple components,
+        e.g., -y 1 -y 3 to extract 1 and 3.
+     -m EXTS1 sampling of first receiver (optional).
+     -n EXTS2 sampling of second receiver (optional).
      -o OUTDIR output directory.
 EOF
 }
@@ -52,7 +54,15 @@ OPTIND=1
 DROP1=1
 DROP2=1
 
-while getopts "hr:s:t:u:e:f:x:y:c:d:n:m:o:" opt; do
+# init samples
+EXTS1=1
+EXTS2=1
+
+# init extraction
+EXTQ1=(0)
+EXTQ2=(0)
+
+while getopts "hr:s:t:u:e:f:x:y:m:n:o:" opt; do
   case "$opt" in
     h)
        show_help
@@ -77,21 +87,15 @@ while getopts "hr:s:t:u:e:f:x:y:c:d:n:m:o:" opt; do
        DROP2=$OPTARG
        ;;
     x)
-       EXTQ1=$OPTARG
+       EXTQ1+=("$OPTARG")
        ;;
     y)
-       EXTQ2=$OPTARG
-       ;;
-    c)
-       EXTC1=$OPTARG
-       ;;
-    d)
-       EXTC2=$OPTARG
-       ;;
-    n)
-       EXTS1=$OPTARG
+       EXTQ2+=("$OPTARG")
        ;;
     m)
+       EXTS1=$OPTARG
+       ;;
+    n)
        EXTS2=$OPTARG
        ;;
     o)
@@ -114,14 +118,34 @@ fi
 # print info on the run
 echo "$(date) running tf_misfit_gof, stay tuned.."
 echo "$(date) arguments:"
-echo "$(date)   receiver 1: ${RECEIVER1}"
-echo "$(date)   receiver 2 (used as reference): ${RECEIVER2}"
+echo "$(date)   receiver 1 (used as reference): ${RECEIVER1}"
+echo "$(date)   receiver 2: ${RECEIVER2}"
 
 # save current dir
 CUR_DIR=$(pwd)
 
 # create output directory
 TMP_DIR=$(mktemp -d)
+
+# add default for extracted quantities
+if [[ ${#EXTQ1[@]} == 1 ]]
+then
+  EXTQ1+=(1)
+fi
+if [[ ${#EXTQ2[@]} == 1 ]]
+then
+  EXTQ2+=(1)
+fi
+
+# set number of components
+if [[  ${#EXTQ1[@]} <  ${#EXTQ2[@]} ]]
+then
+  COMP=$( expr ${#EXTQ1[@]} - 1 )
+else
+  COMP=$( expr ${#EXTQ2[@]} - 1 )
+fi
+
+echo "$(date) comparing ${COMP} components of the receivers"
 
 # extract components if required
 if [[ $EXTQ1 != '' ]]
@@ -130,13 +154,13 @@ then
 
   # extract data
   tail -n +${DROP1} ${RECEIVER1} > $EXT_DIR/ext1.csv
-  recvs_extract_cols.py --in_csv $EXT_DIR/ext1.csv --cols 0 ${EXTQ1} --out_csv ${TMP_DIR}/S1.dat
+  recvs_extract_cols.py --in_csv $EXT_DIR/ext1.csv --cols ${EXTQ1[*]} --sample ${EXTS1} --out_csv ${TMP_DIR}/S2.dat
 
   # remove extraction dir
   rm -r $EXT_DIR
 else
   # drop lines only
-  tail -n +${DROP1} $RECEIVER1 > $TMP_DIR/S1.dat
+  tail -n +${DROP1} $RECEIVER1 > $TMP_DIR/S2.dat
 fi
 
 if [[ $EXTQ2 != '' ]]
@@ -145,13 +169,13 @@ then
 
   # extract data
   tail -n +${DROP2} ${RECEIVER2} > $EXT_DIR/ext2.csv
-  recvs_extract_cols.py --in_csv $EXT_DIR/ext2.csv --cols 0 ${EXTQ2} --out_csv ${TMP_DIR}/S2.dat
+  recvs_extract_cols.py --in_csv $EXT_DIR/ext2.csv --cols ${EXTQ2[*]} --sample ${EXTS2} --out_csv ${TMP_DIR}/S1.dat
 
   # remove extraction dir
   rm -r $EXT_DIR
 else
   # drop lines only
-   tail -n +${DROP2} $RECEIVER2 > $TMP_DIR/S2.dat
+   tail -n +${DROP2} $RECEIVER2 > $TMP_DIR/S1.dat
 fi
 
 # move to temp dir
@@ -159,7 +183,7 @@ cd $TMP_DIR
 
 # create config for TF_MISFIT_GOF
 # TODO: Fix to dynamic number of samples, time step and frequency range.
-echo "&INPUT MT=${NTS}, DT=${DT}, FMIN=0.13, FMAX=5.0, S1_NAME='S1.dat', S2_NAME='S2.dat', NC = 1,
+echo "&INPUT MT=${NTS}, DT=${DT}, FMIN=0.13, FMAX=5.0, S1_NAME='S1.dat', S2_NAME='S2.dat', NC = ${COMP},
        IS_S2_REFERENCE = T, LOCAL_NORM = F/
      "> HF_TF-MISFIT_GOF
 
