259bbb19d4b649f9e6f902848cee0378deff6724
Alexander Nikolas Breuer <anbreuer@ucsd.edu>
2018-07-04 10:48:04 -0700

    Added additional tools for the CI/CD using Debian.

--- tools/gocd/install_tools.sh
+++ tools/gocd/install_tools.sh
@@ -19,10 +19,12 @@
 # THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #
 # @section DESCRIPTION
-# Installs the tools used in CI/CD.
+# Installs the tools used in CI/CD (tested for Debian 9).
 ##
 sudo apt-get install -qq -o=Dpkg::Use-Pty=0 wget
 sudo apt-get install -qq -o=Dpkg::Use-Pty=0 unzip
+sudo apt-get install -qq -o=Dpkg::Use-Pty=0 m4
+sudo apt-get install -qq -o=Dpkg::Use-Pty=0 dh-autoreconf
 sudo apt-get install -qq -o=Dpkg::Use-Pty=0 make
 sudo apt-get install -qq -o=Dpkg::Use-Pty=0 cmake
 sudo apt-get install -qq -o=Dpkg::Use-Pty=0 scons
@@ -33,6 +35,7 @@ sudo apt-get install -qq -o=Dpkg::Use-Pty=0 openmpi-bin
 sudo apt-get install -qq -o=Dpkg::Use-Pty=0 cppcheck
 sudo apt-get install -qq -o=Dpkg::Use-Pty=0 gmsh
 sudo apt-get install -qq -o=Dpkg::Use-Pty=0 gcc
+sudo apt-get install -qq -o=Dpkg::Use-Pty=0 gfortran
 sudo apt-get install -qq -o=Dpkg::Use-Pty=0 clang
 sudo apt-get install -qq -o=Dpkg::Use-Pty=0 valgrind
 sudo pip -q install xmltodict matplotlib
