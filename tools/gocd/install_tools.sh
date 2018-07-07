df33bba303e200545045ef4a0d5c189290ec5a76
Alexander Nikolas Breuer <anbreuer@ucsd.edu>
2018-07-06 22:56:18 -0700

    Fixed assets download in GoCD CI pipeline template. Adjusted git LFS install in GoCD agent tools to Ubuntu 16.

--- tools/gocd/install_tools.sh
+++ tools/gocd/install_tools.sh
@@ -19,7 +19,7 @@
 # THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #
 # @section DESCRIPTION
-# Installs the tools used in CI/CD (tested for Debian 9).
+# Installs the tools used in CI/CD (tested for Debian 9.3, Ubuntu 16.04).
 ##
 sudo apt-get install -qq -o=Dpkg::Use-Pty=0 wget
 sudo apt-get install -qq -o=Dpkg::Use-Pty=0 unzip
@@ -29,7 +29,6 @@ sudo apt-get install -qq -o=Dpkg::Use-Pty=0 make
 sudo apt-get install -qq -o=Dpkg::Use-Pty=0 cmake
 sudo apt-get install -qq -o=Dpkg::Use-Pty=0 scons
 sudo apt-get install -qq -o=Dpkg::Use-Pty=0 git
-sudo apt-get install -qq -o=Dpkg::Use-Pty=0 git-lfs
 sudo apt-get install -qq -o=Dpkg::Use-Pty=0 libxml2-utils
 sudo apt-get install -qq -o=Dpkg::Use-Pty=0 python-pip python3-pip
 sudo apt-get install -qq -o=Dpkg::Use-Pty=0 openmpi-bin
@@ -39,6 +38,8 @@ sudo apt-get install -qq -o=Dpkg::Use-Pty=0 gcc
 sudo apt-get install -qq -o=Dpkg::Use-Pty=0 gfortran
 sudo apt-get install -qq -o=Dpkg::Use-Pty=0 clang
 sudo apt-get install -qq -o=Dpkg::Use-Pty=0 valgrind
+curl -s https://packagecloud.io/install/repositories/github/git-lfs/script.deb.sh | sudo bash
+sudo apt-get install -qq -o=Dpkg::Use-Pty=0 git-lfs
 sudo su go -c "git lfs install"
 sudo pip -q install xmltodict matplotlib netCDF4
 sudo pip3 -q install xmltodict matplotlib netCDF4
