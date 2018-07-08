8e8a9717333744a10d530db5a0b9773a03465d21
Alexander Nikolas Breuer <anbreuer@ucsd.edu>
2018-07-08 15:44:43 -0700

    Refactored one-dimensional f-wave solver for the shallow water equations.

--- src/impl/swe/solvers/Fwave.hpp
+++ src/impl/swe/solvers/Fwave.hpp
@@ -24,102 +24,104 @@
 #ifndef EDGE_SWE_FWAVE_HPP
 #define EDGE_SWE_FWAVE_HPP
 
-#include <algorithm>
-#include "constants.hpp"
-
 namespace edge {
   namespace swe {
     namespace solvers {
+      template< unsigned short TL_N_CRS >
       class Fwave;
     }
   }
 }
 
+/**
+ * @brief One-dimensional f-Wave solver for the shallow water equations.
+ *
+ * @paramt TL_N_CRS number of fused simulations.
+ **/
+template< unsigned short TL_N_CRS >
 class edge::swe::solvers::Fwave {
   public:
     /**
      * Updates the DoFs of an element with contribution of the face.
      *
-     * @param i_hL water height for the left/minus side element.
-     * @param i_hR water height for the right/plus side element.
-     * @param i_huL momentum for left/minus side element.
-     * @param i_huR momentum for right/plus side element.
-     * @param i_bL b for the left/minus side element.
-     * @param i_bR b for the right/plus side element.
+     * @param i_hL water height for the left side element.
+     * @param i_hR water height for the right side element.
+     * @param i_huL momentum for left side element.
+     * @param i_huR momentum for right side element.
+     * @param i_bL b for the left side element.
+     * @param i_bR b for the right side element.
      * @param o_netUpdates set to the net-update of the face.
+     *
+     * @paramt TL_T_REAL floating point precision.
      **/
-    static void computeNetUpdates( const t_elementModePrivate1   i_hL[ N_ELEMENT_MODES][N_CRUNS],
-                                   const t_elementModePrivate1   i_hR[ N_ELEMENT_MODES][N_CRUNS],
-                                   const t_elementModePrivate1   i_huL[N_ELEMENT_MODES][N_CRUNS],
-                                   const t_elementModePrivate1   i_huR[N_ELEMENT_MODES][N_CRUNS],
-                                   const t_elementModeShared1    i_bL,
-                                   const t_elementModeShared1    i_bR,
-                                         t_elementModePrivate1   o_netUpdates[4][N_FACE_MODES][N_CRUNS] ) {
-#if __has_builtin(__builtin_assume_aligned)
-      (void) __builtin_assume_aligned(i_hL, ALIGNMENT.CRUNS);  (void) __builtin_assume_aligned(i_hR, ALIGNMENT.CRUNS);
-      (void) __builtin_assume_aligned(i_huL, ALIGNMENT.CRUNS); (void) __builtin_assume_aligned(i_huR, ALIGNMENT.CRUNS);
-      (void) __builtin_assume_aligned(o_netUpdates, ALIGNMENT.CRUNS);
-#endif
-
+    template< typename TL_T_REAL >
+    static void computeNetUpdates( const TL_T_REAL i_hL[TL_N_CRS],
+                                   const TL_T_REAL i_hR[TL_N_CRS],
+                                   const TL_T_REAL i_huL[TL_N_CRS],
+                                   const TL_T_REAL i_huR[TL_N_CRS],
+                                   const TL_T_REAL i_bL,
+                                   const TL_T_REAL i_bR,
+                                         TL_T_REAL o_netUpdates[4][TL_N_CRS] ) {
+      // gravity constant
+      TL_T_REAL l_g = 9.80665;
       // particle veolocities
-      t_elementModePrivate1 l_uL[N_CRUNS] __attribute__((aligned(ALIGNMENT.BASE.STACK)));
-      t_elementModePrivate1 l_uR[N_CRUNS] __attribute__((aligned(ALIGNMENT.BASE.STACK)));
+      TL_T_REAL l_uL[TL_N_CRS];
+      TL_T_REAL l_uR[TL_N_CRS];
 
       // eigenvalues
-      t_elementModePrivate1 l_lambdaL[N_CRUNS] __attribute__((aligned(ALIGNMENT.BASE.STACK)));
-      t_elementModePrivate1 l_lambdaR[N_CRUNS] __attribute__((aligned(ALIGNMENT.BASE.STACK)));
+      TL_T_REAL l_lambdaL[TL_N_CRS];
+      TL_T_REAL l_lambdaR[TL_N_CRS];
 
       // jump in fluxes
-      t_elementModePrivate1 l_fJump[2][N_CRUNS] __attribute__((aligned(ALIGNMENT.BASE.STACK)));
+      TL_T_REAL l_fJump[2][TL_N_CRS];
 
       // scalar of the matrix inversion
-      t_elementModePrivate1 l_adMbc[N_CRUNS] __attribute__((aligned(ALIGNMENT.BASE.STACK)));
+      TL_T_REAL l_adMbc[TL_N_CRS];
 
       // inverse matrix
-      t_elementModePrivate1 l_iR[2][2][N_CRUNS] __attribute__((aligned(ALIGNMENT.BASE.STACK)));
+      TL_T_REAL l_iR[2][2][TL_N_CRS];
 
       // eigencoefficients
-      t_elementModePrivate1 l_beta[2][N_CRUNS] __attribute__((aligned(ALIGNMENT.BASE.STACK)));
+      TL_T_REAL l_beta[2][TL_N_CRS];
 
       // iterate over concurrent forward runs
-#pragma omp simd
-      for( int_cfr l_run = 0; l_run < N_CRUNS; l_run++ ) {
+      for( unsigned short l_ru = 0; l_ru < TL_N_CRS; l_ru++ ) {
         // particle velocity
-        l_uL[l_run] = i_huL[0][l_run] / i_hL[0][l_run];
-        l_uR[l_run] = i_huR[0][l_run] / i_hR[0][l_run];
+        l_uL[l_ru] = i_huL[l_ru] / i_hL[l_ru];
+        l_uR[l_ru] = i_huR[l_ru] / i_hR[l_ru];
 
         // u -/+ sqrt(g*h)
-        l_lambdaL[l_run] = l_uL[l_run] - std::sqrt( 9.81 * i_hL[0][l_run] );
-        l_lambdaR[l_run] = l_uR[l_run] + std::sqrt( 9.81 * i_hR[0][l_run] );
+        l_lambdaL[l_ru] = l_uL[l_ru] - std::sqrt( l_g * i_hL[l_ru] );
+        l_lambdaR[l_ru] = l_uR[l_ru] + std::sqrt( l_g * i_hR[l_ru] );
 
         // jump in fluxes
-        l_fJump[0][l_run]  = i_huR[0][l_run] - i_huL[0][l_run];
-        l_fJump[1][l_run]  = i_huR[0][l_run] * l_uR[l_run]  + (t_elementModePrivate1) 0.5  * (t_elementModePrivate1) 9.81 * i_hR[0][l_run] * i_hR[0][l_run];
-        l_fJump[1][l_run] -= i_huL[0][l_run] * l_uL[l_run]  + (t_elementModePrivate1) 0.5  * (t_elementModePrivate1) 9.81 * i_hL[0][l_run] * i_hL[0][l_run];
-        l_fJump[1][l_run] += (t_elementModePrivate1) 0.5    * (t_elementModePrivate1) 9.81 * ( i_hR[0][l_run] + i_hL[0][l_run] ) * ( i_bR - i_bL );
+        l_fJump[0][l_ru]  = i_huR[l_ru] - i_huL[l_ru];
+        l_fJump[1][l_ru]  = i_huR[l_ru] * l_uR[l_ru]  + TL_T_REAL(0.5)  * l_g * i_hR[l_ru] * i_hR[l_ru];
+        l_fJump[1][l_ru] -= i_huL[l_ru] * l_uL[l_ru]  + TL_T_REAL(0.5)  * l_g * i_hL[l_ru] * i_hL[l_ru];
+        l_fJump[1][l_ru] += TL_T_REAL(0.5) * l_g * ( i_hR[l_ru] + i_hL[l_ru] ) * ( i_bR - i_bL );
 
         // compute scalar for 2x2 matrix inverse
-        l_adMbc[l_run] = (t_elementModePrivate1) 1.0 / ( l_lambdaR[l_run] - l_lambdaL[l_run] );
+        l_adMbc[l_ru] = TL_T_REAL(1.0) / ( l_lambdaR[l_ru] - l_lambdaL[l_ru] );
 
-        l_iR[0][0][l_run] =  l_adMbc[l_run] * l_lambdaR[l_run];
-        l_iR[0][1][l_run] = -l_adMbc[l_run];
-        l_iR[1][0][l_run] = -l_adMbc[l_run] * l_lambdaL[l_run];
-        l_iR[1][1][l_run] =  l_adMbc[l_run];
+        l_iR[0][0][l_ru] =  l_adMbc[l_ru] * l_lambdaR[l_ru];
+        l_iR[0][1][l_ru] = -l_adMbc[l_ru];
+        l_iR[1][0][l_ru] = -l_adMbc[l_ru] * l_lambdaL[l_ru];
+        l_iR[1][1][l_ru] =  l_adMbc[l_ru];
 
         // compute eigen coefficients
-        l_beta[0][l_run]  = l_iR[0][0][l_run] * l_fJump[0][l_run];
-        l_beta[0][l_run] += l_iR[0][1][l_run] * l_fJump[1][l_run];
+        l_beta[0][l_ru]  = l_iR[0][0][l_ru] * l_fJump[0][l_ru];
+        l_beta[0][l_ru] += l_iR[0][1][l_ru] * l_fJump[1][l_ru];
 
-        l_beta[1][l_run]  = l_iR[1][0][l_run] * l_fJump[0][l_run];
-        l_beta[1][l_run] += l_iR[1][1][l_run] * l_fJump[1][l_run];
+        l_beta[1][l_ru]  = l_iR[1][0][l_ru] * l_fJump[0][l_ru];
+        l_beta[1][l_ru] += l_iR[1][1][l_ru] * l_fJump[1][l_ru];
 
         // update DOFs
         // TODO: This fails in supercritical states!
-        o_netUpdates[0][0][l_run] = l_beta[0][l_run];
-        o_netUpdates[1][0][l_run] = l_beta[0][l_run] * l_lambdaL[l_run];
+        o_netUpdates[0][l_ru] = l_beta[0][l_ru];
+        o_netUpdates[1][l_ru] = l_beta[0][l_ru] * l_lambdaL[l_ru];
 
-        o_netUpdates[2][0][l_run] = l_beta[1][l_run];
-        o_netUpdates[3][0][l_run] = l_beta[1][l_run] * l_lambdaR[l_run];
+        o_netUpdates[2][l_ru] = l_beta[1][l_ru];
+        o_netUpdates[3][l_ru] = l_beta[1][l_ru] * l_lambdaR[l_ru];
       }
     }
 };
