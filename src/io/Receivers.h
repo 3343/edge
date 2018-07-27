7f6ff4bab8828d91fe6c16df02df2145a5af25ab
Alexander Nikolas Breuer <anbreuer@ucsd.edu>
2018-07-27 11:55:33 -0700

    Added support for receivers outside of the mesh, which are projected to the boundaries. Added OpenMP parallelization to the receiver search.

--- src/io/Receivers.h
+++ src/io/Receivers.h
@@ -4,7 +4,7 @@
  * @author Alexander Breuer (anbreuer AT ucsd.edu)
  *
  * @section LICENSE
- * Copyright (c) 2016, Regents of the University of California
+ * Copyright (c) 2016-2018, Regents of the University of California
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
@@ -37,6 +37,8 @@ namespace edge {
 class edge::io::Receivers {
   protected:
     struct Recv {
+      //! coordinates of the receivers
+      double coords[3];
       //! number of buffered values
       unsigned int nBuff;
       //! buffer
