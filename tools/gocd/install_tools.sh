38cc5a93a90b5e89ee036f7b5e1a9a2e1f3e5034
Alexander Nikolas Breuer <anbreuer@ucsd.edu>
2018-09-27 15:46:11 -0700

    Updated GoCD-tools.

--- tools/gocd/install_tools.sh
+++ tools/gocd/install_tools.sh
@@ -21,28 +21,52 @@
 # @section DESCRIPTION
 # Installs the tools used in CI/CD (tested for Debian 9.3, Ubuntu 16.04).
 ##
-sudo apt-get install -qq -o=Dpkg::Use-Pty=0 wget
-sudo apt-get install -qq -o=Dpkg::Use-Pty=0 unzip
-sudo apt-get install -qq -o=Dpkg::Use-Pty=0 m4
-sudo apt-get install -qq -o=Dpkg::Use-Pty=0 dh-autoreconf
-sudo apt-get install -qq -o=Dpkg::Use-Pty=0 make
-sudo apt-get install -qq -o=Dpkg::Use-Pty=0 cmake
-sudo apt-get install -qq -o=Dpkg::Use-Pty=0 scons
-sudo apt-get install -qq -o=Dpkg::Use-Pty=0 git
-sudo apt-get install -qq -o=Dpkg::Use-Pty=0 libxml2-utils
-sudo apt-get install -qq -o=Dpkg::Use-Pty=0 python-pip python3-pip
-sudo apt-get install -qq -o=Dpkg::Use-Pty=0 openmpi-bin libopenmpi-dev
-sudo apt-get install -qq -o=Dpkg::Use-Pty=0 cppcheck
-sudo apt-get install -qq -o=Dpkg::Use-Pty=0 gmsh
-sudo apt-get install -qq -o=Dpkg::Use-Pty=0 gcc
-sudo apt-get install -qq -o=Dpkg::Use-Pty=0 gfortran
-sudo apt-get install -qq -o=Dpkg::Use-Pty=0 clang
-sudo apt-get install -qq -o=Dpkg::Use-Pty=0 libiomp5 libiomp-dev
-sudo apt-get install -qq -o=Dpkg::Use-Pty=0 valgrind
+sudo apt-get install -qq -o=Dpkg::Use-Pty=0 -y wget
+sudo apt-get install -qq -o=Dpkg::Use-Pty=0 -y unzip
+sudo apt-get install -qq -o=Dpkg::Use-Pty=0 -y m4
+sudo apt-get install -qq -o=Dpkg::Use-Pty=0 -y dh-autoreconf
+sudo apt-get install -qq -o=Dpkg::Use-Pty=0 -y make
+sudo apt-get install -qq -o=Dpkg::Use-Pty=0 -y cmake
+sudo apt-get install -qq -o=Dpkg::Use-Pty=0 -y scons
+sudo apt-get install -qq -o=Dpkg::Use-Pty=0 -y git
+sudo apt-get install -qq -o=Dpkg::Use-Pty=0 -y libxml2-utils
+sudo apt-get install -qq -o=Dpkg::Use-Pty=0 -y python-pip python3-pip
+sudo apt-get install -qq -o=Dpkg::Use-Pty=0 -y openmpi-bin libopenmpi-dev
+sudo apt-get install -qq -o=Dpkg::Use-Pty=0 -y cppcheck
+sudo apt-get install -qq -o=Dpkg::Use-Pty=0 -y gmsh
+sudo apt-get install -qq -o=Dpkg::Use-Pty=0 -y gcc
+sudo apt-get install -qq -o=Dpkg::Use-Pty=0 -y gfortran
+sudo apt-get install -qq -o=Dpkg::Use-Pty=0 -y clang
+sudo apt-get install -qq -o=Dpkg::Use-Pty=0 -y libiomp5 libiomp-dev
+sudo apt-get install -qq -o=Dpkg::Use-Pty=0 -y valgrind
+sudo apt-get install -qq -o=Dpkg::Use-Pty=0 -y software-properties-common
+
+# install clang
+sudo echo "deb http://apt.llvm.org/stretch/ llvm-toolchain-stretch-7 main"     >  /etc/apt/sources.list.d/clang.list
+sudo echo "deb-src http://apt.llvm.org/stretch/ llvm-toolchain-stretch-7 main" >> /etc/apt/sources.list.d/clang.list
+sudo wget -O - https://apt.llvm.org/llvm-snapshot.gpg.key|sudo apt-key add -
+sudo apt-get update
+sudo apt-get install -qq -o=Dpkg::Use-Pty=0 -y clang-7 lldb-7 lld-7
+
+# install valgrind
+sudo apt-get install -qq -o=Dpkg::Use-Pty=0 libc6-dbg
+wget http://mirrors.kernel.org/sourceware/valgrind/valgrind-3.13.0.tar.bz2
+tar -xjf valgrind-3.13.0.tar.bz2
+cd valgrind-3.13.0
+./configure
+make
+sudo make install
+cd ..
+
+# install Git LFS
 curl -s https://packagecloud.io/install/repositories/github/git-lfs/script.deb.sh | sudo bash
-sudo apt-get install -qq -o=Dpkg::Use-Pty=0 git-lfs
+sudo apt-get install -qq -o=Dpkg::Use-Pty=0 -y git-lfs
 sudo su go -c "git lfs install"
+
+# install python modules
 sudo pip -q install xmltodict matplotlib netCDF4
 sudo pip3 -q install xmltodict matplotlib netCDF4
+
+# install Vagrant
 wget https://releases.hashicorp.com/vagrant/2.1.2/vagrant_2.1.2_x86_64.deb -O vagrant.deb
 sudo dpkg -i vagrant.deb
