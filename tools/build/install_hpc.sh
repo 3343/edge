c8aef83a96e1276426c630d421aba6004dbc46b7
Alexander Nikolas Breuer <anbreuer@ucsd.edu>
2018-10-30 23:54:55 -0700

    Switched to dynamic installation of SCORE-P and Scalasca in HPC tools builds. Switched to default Intel-wrappers for SCORE-P and Scalasca builds in HPC tools script.

--- tools/build/install_hpc.sh
+++ tools/build/install_hpc.sh
@@ -73,9 +73,9 @@ mkdir scorep; tar -xzf scorep.tar.gz -C scorep --strip-components=1
 cd scorep
 if [[ $(type -P mpiicc) ]] && [[ $(type -P mpiicpc) ]] && [[ $(type -P mpiifort) ]]
 then
-  ./configure --with-mpi=intel3 --with-nocross-compiler-suite=intel
+  ./configure --disable-static --enable-shared --with-mpi=intel --with-nocross-compiler-suite=intel
 else
-  ./configure
+  ./configure --disable-static --enable-shared
 fi
 make -j ${EDGE_N_BUILD_PROC}
 sudo make install
@@ -90,9 +90,9 @@ mkdir scalasca; tar -xzf scalasca.tar.gz -C scalasca --strip-components=1
 cd scalasca
 if [[ $(type -P mpiicc) ]] && [[ $(type -P mpiicpc) ]] && [[ $(type -P mpiifort) ]]
 then
-  ./configure --with-mpi=intel3 --with-nocross-compiler-suite=intel
+  ./configure --disable-static --enable-shared --with-mpi=intel --with-nocross-compiler-suite=intel
 else
-  ./configure
+  ./configure --disable-static --enable-shared
 fi
 make -j ${EDGE_N_BUILD_PROC}
 sudo make install
