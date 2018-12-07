25a5e58b9d683b8509a9e6ed139a65aff583e484
Alexander Nikolas Breuer <anbreuer@ucsd.edu>
2018-12-07 14:56:48 -0800

    Fixed TCP setup.

--- tools/build/install_hpc.sh
+++ tools/build/install_hpc.sh
@@ -145,15 +145,15 @@ then
   sudo bash -c "echo net.core.rmem_max = 16777216 >> /etc/sysctl.conf"
   sudo bash -c "echo net.core.wmem_max = 16777216 >> /etc/sysctl.conf"
 
-  sudo bash -c "echo net.ipv4.tcp_rmem = 4096 87380 16777216"
-  sudo bash -c "echo net.ipv4.tcp_wmem = 4096 65536 16777216"
+  sudo bash -c "echo net.ipv4.tcp_rmem = 4096 87380 16777216 >> /etc/sysctl.conf"
+  sudo bash -c "echo net.ipv4.tcp_wmem = 4096 65536 16777216 >> /etc/sysctl.conf"
 
   sudo bash -c "echo net.core.netdev_max_backlog = 30000 >> /etc/sysctl.conf"
 
   sudo bash -c "echo net.core.rmem_default = 16777216 >> /etc/sysctl.conf"
   sudo bash -c "echo net.core.wmem_default = 16777216 >> /etc/sysctl.conf"
 
-  sudo bash -c "echo net.ipv4.tcp_mem = 16777216 16777216 16777216"
+  sudo bash -c "echo net.ipv4.tcp_mem = 16777216 16777216 16777216 >> /etc/sysctl.conf"
 
   sudo bash -c "echo net.ipv4.route.flush = 1"
 
