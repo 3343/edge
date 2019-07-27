21c1f07d901945ddea5b17f50fa1511d7057c092
Alexander Nikolas Breuer <breuer@mytum.de>
2019-07-27 12:19:31 +0200

    Fixed installation of llvm-toolset in tools build script.

--- tools/build/install_tools.sh
+++ tools/build/install_tools.sh
@@ -82,10 +82,10 @@ sudo tar -xf gmsh.tgz -C /usr --strip-components=1
 #########
 if [[ ${EDGE_DIST} == *"CentOS"* ]]
 then
-  sudo yum install -y -q -e 0 llvm-toolset-8-clang llvm-toolset-8-libomp llvm-toolset-8-libomp-devel
+  sudo yum install -y -q -e 0 llvm-toolset-7-clang llvm-toolset-7-libomp llvm-toolset-7-libomp-devel
   sudo yum install -y -q -e 0 clang
-  source /opt/rh/llvm-toolset-8/enable
-  echo "source /opt/rh/llvm-toolset-8/enable > /dev/null" | sudo tee --append /etc/bashrc
+  source /opt/rh/llvm-toolset-7/enable
+  echo "source /opt/rh/llvm-toolset-7/enable > /dev/null" | sudo tee --append /etc/bashrc
 fi
 
 ###########
