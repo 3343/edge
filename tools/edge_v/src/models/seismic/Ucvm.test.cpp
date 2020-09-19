8e75c4a3402d77699ccb7d0aee5f6b9d60adf39a
Alexander Nikolas Breuer <breuer@mytum.de>
2020-09-19 16:19:02 +0200

    Switched unit tests to cvmsi.

--- tools/edge_v/src/models/seismic/Ucvm.test.cpp
+++ tools/edge_v/src/models/seismic/Ucvm.test.cpp
@@ -57,27 +57,27 @@ TEST_CASE( "Tests the UCVM model abstraction.", "[ucvm][model]" ) {
                l_pts );
 
   // check results (analogue to io unit tests)
-  REQUIRE( l_ucvm.m_velP[0] == Approx( 696.491) );
-  REQUIRE( l_ucvm.m_velP[1] == Approx(1669.540) );
-  REQUIRE( l_ucvm.m_velP[2] == Approx(1683.174) );
-  REQUIRE( l_ucvm.m_velP[3] == Approx(3097.562) );
-  REQUIRE( l_ucvm.m_velP[4] == Approx(3660.809) );
+  REQUIRE( l_ucvm.m_velP[0] == Approx(  696.491 ) );
+  REQUIRE( l_ucvm.m_velP[1] == Approx( 1669.540 ) );
+  REQUIRE( l_ucvm.m_velP[2] == Approx( 1683.174 ) );
+  REQUIRE( l_ucvm.m_velP[3] == Approx( 2701.216 ) );
+  REQUIRE( l_ucvm.m_velP[4] == Approx( 3330.909 ) );
 
-  REQUIRE( l_ucvm.m_velS[0] == Approx(  213.000) );
-  REQUIRE( l_ucvm.m_velS[1] == Approx(  548.000) );
-  REQUIRE( l_ucvm.m_velS[2] == Approx(  603.470) );
-  REQUIRE( l_ucvm.m_velS[3] == Approx( 1656.495) );
-  REQUIRE( l_ucvm.m_velS[4] == Approx( 2056.628) );
+  REQUIRE( l_ucvm.m_velS[0] == Approx(  213.000 ) );
+  REQUIRE( l_ucvm.m_velS[1] == Approx(  548.000 ) );
+  REQUIRE( l_ucvm.m_velS[2] == Approx(  603.470 ) );
+  REQUIRE( l_ucvm.m_velS[3] == Approx( 1475.608 ) );
+  REQUIRE( l_ucvm.m_velS[4] == Approx( 1945.594 ) );
 
-  REQUIRE( l_ucvm.m_rho[0] == Approx(1974.976) );
-  REQUIRE( l_ucvm.m_rho[1] == Approx(2128.620) );
-  REQUIRE( l_ucvm.m_rho[2] == Approx(2130.773) );
-  REQUIRE( l_ucvm.m_rho[3] == Approx(2354.105) );
-  REQUIRE( l_ucvm.m_rho[4] == Approx(2443.042) );
+  REQUIRE( l_ucvm.m_rho[0] == Approx( 1974.976 ) );
+  REQUIRE( l_ucvm.m_rho[1] == Approx( 2128.620 ) );
+  REQUIRE( l_ucvm.m_rho[2] == Approx( 2130.773 ) );
+  REQUIRE( l_ucvm.m_rho[3] == Approx( 2354.105 ) );
+  REQUIRE( l_ucvm.m_rho[4] == Approx( 2443.042 ) );
 
-  REQUIRE( l_ucvm.getMaxSpeed(0) == Approx( 696.491) );
-  REQUIRE( l_ucvm.getMaxSpeed(1) == Approx(1669.540) );
-  REQUIRE( l_ucvm.getMaxSpeed(2) == Approx(1683.174) );
-  REQUIRE( l_ucvm.getMaxSpeed(3) == Approx(3097.562) );
-  REQUIRE( l_ucvm.getMaxSpeed(4) == Approx(3660.809) );
+  REQUIRE( l_ucvm.getMaxSpeed(0) == Approx(  696.491 ) );
+  REQUIRE( l_ucvm.getMaxSpeed(1) == Approx( 1669.540 ) );
+  REQUIRE( l_ucvm.getMaxSpeed(2) == Approx( 1683.174 ) );
+  REQUIRE( l_ucvm.getMaxSpeed(3) == Approx( 2701.216 ) );
+  REQUIRE( l_ucvm.getMaxSpeed(4) == Approx( 3330.909 ) );
 }
